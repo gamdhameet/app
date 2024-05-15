3e9aaac9b89597d611f4bce43e20eaaaff4b2705
Meet Gamdha <gamdhameet@gmail.com>
2024-05-15 11:46:15 +0530

    2

--- app/settings.py
+++ app/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIR = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
